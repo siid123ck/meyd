07167e6139f7c1e762af4f68f3cd3f3468b9a32f
siid sharma <siid123ck@gmail.com>
2023-04-28 12:09:20 +1000

    initialized the project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World from siid!');
+});
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
+});
